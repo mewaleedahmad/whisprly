e9a1dfc41dc961dc07c2da994172ac3cdd941dce
Waleed Gondal <waleedgondal57@gmail.com>
2024-11-04 22:08:48 +0500

    Initial folder structure

---
+++ server/server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import dotenv  from 'dotenv';
+const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT || 5000 
+app.get('/', (req, res) => {
+    res.send('Hello, World!')
+})
+
+app.listen(PORT,()=>{
+    console.log(`App is running on ${PORT}`)
+})
