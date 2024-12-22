560b8de3e884e1a23b0237666da0c08e55c43521
Waleed Gondal <waleedgondal57@gmail.com>
2024-12-22 23:10:22 +0500

    Auth Signup Bug Fixed

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -8,9 +8,13 @@ export const signup = async(req,res)=>{
         if (password !== confirmPassword) {
             res.status(400).json({error: 'Passwords do not match'})
         }
-        const userExists = await userModel.findOne({email})
-        if (userExists) {
-           return res.status(400).json({error: 'User already exists'})
+        const checkUserExists = await userModel.findOne({email})
+        if (checkUserExists) {
+           return res.status(400).json({message: 'A user with this email already exists.'})
+        }
+        const checkUniqueUsername = await userModel.findOne({userName})
+        if(checkUniqueUsername) {
+            return res.status(400).json({message: 'Username must be unique'})
         }
         
         const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${userName}`
@@ -31,7 +35,13 @@ export const signup = async(req,res)=>{
         if(newUser){
             generateTokenAndSetCookie(newUser._id,res)
             await newUser.save()
-            res.status(200).json({message: "Account Created"})
+            res.status(200).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                userName : newUser.username,
+                profilePic: newUser.profilePic,
+                message: "Account Created Successfully"
+            })
         }
 
     } catch (error) {
