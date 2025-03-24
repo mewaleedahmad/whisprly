b6de4f9081c1605387374bd3d1ae88a1483934e8
Waleed-Gondal <waleedgondal57@gmail.com>
2025-03-24 21:59:14 +0500

    updates

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,25 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server:{
-    port : 3000,
-    host: true,
-    proxy:{
-      '/api':{ 
-        target: 'http://localhost:8000',
-        changeOrigin: true
-      },
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current directory
+  const env = loadEnv(mode, process.cwd())
+  
+  return {
+    plugins: [react()],
+    server:{
+      port : 3000,
+      host: true,
+      proxy: {
+        '/api': {
+          target: env.VITE_MODE === "development" 
+            ? "http://localhost:8000" 
+            : env.VITE_API_URL,
+          changeOrigin: true,
+          secure: true, 
+        },
+      }
     }
   }
 })
